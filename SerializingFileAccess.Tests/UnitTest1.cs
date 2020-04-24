fdef2fa04c55e5370c6bba930f57c40728280000
Olo32 <olo@apck.de>
2020-04-24 18:09:32 +0200

    Add Tests project

---
+++ SerializingFileAccess.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+// Copyright (c) Oliver Appel. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using NUnit.Framework;
+
+namespace Olo42.SerializingFileAccess.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
