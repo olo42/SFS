3d6757d8a156f5ff9b307df5a2edaad67153386a
Olo32 <olo@apck.de>
2020-04-24 17:12:27 +0200

    Add Abstractions project

---
+++ SerializingFileAccess.Abstractions/ISerializingFileAccess.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Oliver Appel. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace olo42.SerializingFileAccess.Abstractions
+{
+  public interface ISerializingFileAccess
+  {
+    
+  }
+}
