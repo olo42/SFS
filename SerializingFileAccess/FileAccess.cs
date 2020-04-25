ceba5a18d9721ac1c655156ac945363252b0a4c4
Olo32 <olo@apck.de>
2020-04-25 14:16:53 +0200

    Add SerializingFileAccess project

---
+++ SerializingFileAccess/FileAccess.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Oliver Appel. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using Olo42.SerializingFileAccess.Abstractions;
+
+namespace SerializingFileAccess
+{
+  public class FileAccess<T> : ISerializingFileAccess<T>
+  {
+    public void Create(T obj)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void Delete()
+    {
+      throw new NotImplementedException();
+    }
+
+    public T Read()
+    {
+      throw new NotImplementedException();
+    }
+
+    public void Update(T obj)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
