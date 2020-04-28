60dda912e560e98fdb1e5db226cf41be577a8fef
Olo32 <olo@apck.de>
2020-04-28 22:53:39 +0200

    Add FilesystemAccess class & tests

---
+++ FileAccess/Filesystem.Tests/FilesystemFileAccessTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Olo42.SFS.FileAccess.Fileystem.Tests
+{
+    public class FilesystemAccessTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
