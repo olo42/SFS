631d59b4f357e60295657639abad8648b007deb6
Olo32 <olo@apck.de>
2020-05-05 11:36:45 +0200

    Use Uri in IRepository

--- Repository/Abstractions/IRepository.cs
+++ Repository/Abstractions/IRepository.cs
@@ -1,16 +1,17 @@
 ﻿// Copyright (c) Oliver Appel. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.Threading.Tasks;
 
 namespace Olo42.SFS.Repository.Abstractions
 {
   public interface IRepository<T>
   {
-    Task Write(T obj);
+    Task Write(Uri path, T obj);
 
-    Task<T> Read();
+    Task<T> Read(Uri path);
 
-    Task Delete();
+    Task Delete(Uri path);
   }
 }
